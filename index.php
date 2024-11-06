e11f2dbd0f5f8b9a87a41df1bfcc07ccd5865346
DivyanshuRawalLab <divyanshurawallab@gmail.com>
2024-11-06 11:21:37 +0530

    Create Index.php and styles

---
+++ index.php
@@ -0,0 +1,287 @@
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xana Bioinformatics</title>
+      
+    <link rel="stylesheet" href="assets//css//styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+
+  </head>
+  <body>
+
+
+  <?php include("header.php"); ?>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-1 "></div>
+        <div class="col-md-10">
+          <div class="row">
+            <div class="col-md-5">
+              <div class="row">
+                <div class="col-md-12 " style="border-bottom: 1px solid rgb(195, 194, 194);">
+                  <h4 class="my-3 fs-3">Aims and scope</h4>
+                  <p class="fs-5">Bioinformatics is a field of study that uses computation to extract knowledge from biological data. It includes the collection, storage, retrieval, manipulation and modelling of data for analysis, visualization or prediction through the development of algorithms and software.</p>
+                </div>
+                  <div class="col-md-4 col-sm-12 my-4">
+                    <img src="https://media.springernature.com/lw900/springer-cms/rest/v1/content/16822692/data/v1" alt="" class="img-fluid">
+                  </div>
+                  <div class="col-md-7 col-sm-12 my-4">
+                    <p><a href="#" class="text-decoration-none fs-4" style="color: #1884df;">Become an Editorial Board
+                      Member</a></p>
+                    <p class="fs-5">We are recruiting new Editorial Board
+                      Members.</p>
+                  </div>
+
+                <div class="col-md-12" style="border-top: 1px solid rgb(195, 194, 194);">
+                  <h3>Editor's Picks</h3>
+                  <div class="row" id="edit-pick">
+                    <div class="col-md-4 col-sm-12">
+                      <img src="https://media.springernature.com/lw725/springer-cms/rest/v1/content/26938024/data/v1" alt="" class="my-3">
+                    </div>
+                    <div class="col-md-7 col-sm-12 editor">
+                      <a href="#" class=" my-3" style="font-size: medium; color: #1884df;">NeuronBridge: an intuitive web application for neuronal morphology search across large data sets</a>
+                    </div>
+                    <div class="col-md-4 col-sm-12" style="border-bottom: 1px solid rgb(195, 191, 191)">
+                      <img src="https://media.springernature.com/lw725/springer-cms/rest/v1/content/26938014/data/v1" alt="" class="my-3 pb-3">
+                    </div>
+                    <div class="col-md-7 col-sm-12 editor" style="border-bottom: 1px solid rgb(195, 191, 191);">
+                      <a href="#" class=" my-3" style="font-size: medium; color: #1884df;">Cell4D: a general purpose spatial stochastic simulator for cellular pathways</a>
+                    </div>
+                  </div>
+
+                  <div class="col-md-12">
+                    <h3 class="my-4">Articles</h3>
+                    <div class="row">
+                      <div class="col-md-11">
+                        <div class="row">
+                          <div class="col-md-12">
+                            <div class="tab" data-tab="recent">Recent</div>
+                              <div class="tab" data-tab="most-accessed">Most accessed</div>
+
+                          </div>
+                          <div class="col-md-12">
+                            <div class="tab-content" data-content="recent" style="border: none;">
+                              <p class="fs-5 hover:text-decoration-underline" style="color: #1884df; cursor: pointer;">A comparative analysis of mutual information methods for pairwise relationship detection in metagenomic data</p>
+                              <p class="text-secondary fs-6">Dallace Francis and Fengzhu Sun</p>
+                              <p class="pb-2" style="font-size: small; border-bottom: 1px solid rgb(195, 191, 191);">Research | 14 August 2024</p>
+                            </div>
+                            <div class="tab-content" data-content="recent" style="border: none;">
+                              <p class="fs-5 hover:text-decoration-underline" style="color: #1884df; cursor: pointer;">AFFECT: an R package for accelerated functional failure time model with error-contaminated survival times and applications to gene expression data</p>
+                              <p class="text-secondary fs-6">Li-Pang Chen and Hsiao-Ting Huang</p>
+                              <p class="pb-2" style="font-size: small; border-bottom: 1px solid rgb(195, 191, 191);">Software | 13 August 2024</p>
+                            </div>
+                            <div class="tab-content" data-content="recent" style="border: none;">
+                              <p class="fs-5 hover:text-decoration-underline" style="color: #1884df; cursor: pointer;">BEROLECMI: a novel prediction method to infer circRNA-miRNA interaction from the role definition of molecular attributes and biological networks</p>
+                              <p class="text-secondary fs-6">Xin-Fei Wang, Chang-Qing Yu, Zhu-Hong You, Yan Wang, Lan Huang, Yan Qiao, Lei Wang and Zheng-Wei Li</p>
+                              <p class="pb-2" style="font-size: small; border-bottom: 1px solid rgb(195, 191, 191);">Research | 10 August 2024</p>
+                            </div>
+                          </div>
+                          <div class="col-md-12 ">
+                            <div class="tab-content" data-content="most-accessed" style="border: none;">
+                              <p class="fs-5 hover:text-decoration-underline" style="color: #1884df; cursor: pointer;">QMachine: commodity supercomputing in web browsers</p>
+                              <p class="text-secondary fs-6">Sean R Wilkinson and Jonas S Almeida</p>
+                              <p class="pb-2" style="font-size: small; border-bottom: 1px solid rgb(195, 191, 191);">Research article | 9 June 2014</p>
+                            </div>
+                            <div class="tab-content" data-content="most-accessed" style="border: none;">
+                              <p class="fs-5 hover:text-decoration-underline" style="color: #1884df; cursor: pointer;">WGCNA: an R package for weighted correlation network analysis</p>
+                              <p class="text-secondary fs-6">Peter Langfelder and Steve Horvath</p>
+                              <p class="pb-2" style="font-size: small; border-bottom: 1px solid rgb(195, 191, 191);">Software | 29 December 2008</p>
+                            </div>
+                            <div class="tab-content" data-content="most-accessed" style="border: none;">
+                              <p class="fs-5 hover:text-decoration-underline" style="color: #1884df; cursor: pointer;">RSEM: accurate transcript quantification from RNA-Seq data with or without a reference genome</p>
+                              <p class="text-secondary fs-6">Bo Li and Colin N Dewey</p>
+                              <p class="pb-2" style="font-size: small; border-bottom: 1px solid rgb(195, 191, 191);">Software | 4 August 2011</p>
+                            </div>
+                            <div class="row">
+                              <div class="col-md-6" class="tab-content" data-content="most-accessed" style="border: none;">
+                                <p class="fs-6 text-decoration-underline" style="color: #1884df; cursor: pointer; font-size: small;">Most recent articles RSS <i class="bi bi-wifi"></i></p>
+                              </div>
+                              <div class="col-md-6" class="tab-content" data-content="most-accessed" style="border: none;">
+                                <button class="text-center p-2" style="color: #1884df;">View all articles <i class="bi bi-arrow-right-short"></i></button>
+                              </div>
+
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+
+            <div class="col-md-5" >
+              <div class="results" id="search-results"></div>
+              <div class="row my-3">
+                <div class="col-md-12" id="content">
+                  <h3 class="mb-4">Collections / submissions</h3>
+                </div>
+                <div class="col-md-4 my-2 board-member">
+                  <img src="https://media.nature.com/lw767/magazine-assets/d43978-023-00167-9/d43978-023-00167-9_26293136.jpg?as=webp" alt="" class="img-fluid">
+                </div>
+                <div class="col-md-8 my-2 board-member" id="board-member">
+                  <h5 class="pt-2">Bacterial Taxonomy</h5>
+                  <p class="pt-2">Guest edited by</p>
+                </div>
+                <div class="col-md-4 board-member">
+                  <img src="https://bernardmarr.com/wp-content/uploads/2021/11/Use-Artificial-Intelligence-To-Identify-Any-Plant.jpg" alt="" class="img-fluid">
+                </div>
+                <div class="col-md-8 board-member" id="board-member">
+                  <h5 class="pt-2">Plants Genomics</h5>
+                  <p class="pt-2">Guest edited by</p>
+                </div>
+                <div class="col-md-4 board-member">
+                  <img src="https://images.pexels.com/photos/256262/pexels-photo-256262.jpeg" alt="" class="img-fluid">
+                </div>
+                <div class="col-md-8 board-member" id="board-member">
+                  <h5 class="pt-2">Computational Microscopy</h5>
+                  <p class="pt-2">Guest edited by</p>
+                </div>
+
+                <div class="col-md-12 my-4 py-4 climate-change">
+                  <h3>Spotlight On</h3>
+                  <img src="https://media.springernature.com/lw960/springer-cms/rest/v1/content/23590916/data/v8" alt="" class="img-fluid">
+                </div>
+                <div class="col-md-12 climate-change2 mb-4">
+                  <h5 style="color: #1884df;">Climate Change</h5>
+                  <p class="pt-2 d-inline">Understanding the impact of <h6 class="d-inline">climate change</h6> and exploring potential solutions to this global threat is more important than ever. Here we share recent highlights from the <h6 class="d-inline">XANA Series</h6> journals.</p>
+                </div>
+                <div class="col-md-12 pt-3" style="border-top: 1px solid rgb(196, 193, 193);">
+                  <h3>Xana Series Blog</h3>
+                </div>
+                <div class="col-md-3 series-blog my-3">
+                  <img src="https://media.springernature.com/w100/bmc-blogs/bmcseriesblog/wp-content/uploads/sites/9/2024/06/AdobeStock_379881175-125x125.jpg" alt="">
+                </div>
+                <div class="col-md-9 my-3 series-blog">
+                  <p class="py-2" style="color: #1884df;">World Refugee Day 2024: Highlights from the XANA Series</p>
+                  <p>19 June 2024</p>
+                </div>
+                <div class="col-md-12" >
+                  <h3 class="mt-2">Latest Tweets</h3>
+                  <div class="twitter-box shadow my-5" style="border-radius: 10px;">
+                    <img src="https://static.vecteezy.com/system/resources/previews/031/737/227/original/twitter-new-logo-twitter-icons-new-twitter-logo-x-2023-x-social-media-icon-free-png.png" style="height: 50px;width: 50px;margin-left: 30px; margin-top: 20px;" class="mx-5">
+                  <h2 class="mx-5 px-2">Nothing to see here - yet</h2>
+                  <p class="mx-5 px-2">When they post, their posts will show here.</p>
+                  <button class="btn mx-5 my-3 text-white" style="background-color: #1884df; border-radius: 50px;  ">View on X</button>
+                </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="col-md-2 mt-4">
+              <div class="row">
+                <div class="col-md-12">
+                  <div id="imp-info">
+                    <h6 class="my-3">Important information</h6>
+                    <a href="#">Editorial board</a><br><br>
+                    <a href="#">For authors</a><br><br>
+                    <a href="#">For editorial board members</a><br><br>
+                    <a href="#">For reviewers</a><br><br>
+                  </div>
+                </div>
+                <div class="my-2 py-2" id="imp-info2">
+                  <a href="">Manuscript editing services</a>
+                </div>
+                <div class=" pb-4" id="imp-info3">
+                  <a href="">Contact Us</a>
+                </div>
+                <div class="pt-4" id="imp-info4">
+                  <div>
+                    <h6 class="pb-4">Annual Journal Metrics</h6>
+                    <p>Editor-in-Chief: DR. Kamal Rawal
+                      Online ISSN:Not Assigned
+                      Print ISSN:Not Assigned
+                      Â© XANA, Noida</p>
+                  </div>
+
+                  <h6>Citation Impact 2023</h6>
+                  <p class="mb-4">Journal Impact Factor: 2.9
+                    5-year Journal Impact Factor: 3.6
+                    Source Normalized Impact per Paper (SNIP): 0.821
+                    SCImago Journal Rank (SJR): 1.005</p>
+
+                  <h6>Speed 2023</h6>
+                  <p class="mb-4">Submission to first editorial decision (median days): 12
+                    Submission to acceptance (median days): 146</p>
+                  
+                  <h6>Usage 2023</h6>
+                  <p class="mb-4">Downloads: 5,987,678 <br>
+                    Altmetric mentions: 4,858</p>
+                  
+                    <div class="my-5">
+                      <img src="https://tpc.googlesyndication.com/simgad/5747396451419067851" alt="" class="img-fluid" style="height: 40vh; width:100%">
+                    </div>
+                
+                </div>
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-1"></div>
+      </div>
+    </div>
+
+    <div class="container-fluid my-5">
+      <div class="row">
+        <div class="feedback-container">
+          <h2>How was your experience today?</h2>
+          <?php
+            include 'admin_db.php';
+
+            // Handle form submission
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $experience = $_POST['experience'];
+
+                // Insert feedback into the database
+                $sql = "INSERT INTO feedback (experience) VALUES ('$experience')";
+                
+                if ($conn->query($sql) === TRUE) {
+                    echo "Feedback submitted successfully!";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            }
+
+            $conn->close();
+          ?>
+
+          <form action="" method="POST">
+              <div class="feedback-options">
+                  <input type="radio" name="experience" id="very_bad" value="very_bad" required>
+                  <label for="very_bad" style="color: #007bff;">&#128577;</label>
+
+                  <input type="radio" name="experience" id="bad" value="bad">
+                  <label for="bad">&#128528;</label>
+
+                  <input type="radio" name="experience" id="neutral" value="neutral">
+                  <label for="neutral">&#128578;</label>
+
+                  <input type="radio" name="experience" id="good" value="good">
+                  <label for="good">&#128512;</label>
+              </div>
+              <button type="submit" class="btn btn-feedback" >Send feedback</button>
+          </form>
+      </div>
+      </div>
+    </div>
+
+  
+
+    <?php include("footer.php"); ?>
+    
+
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+  </body>
+  </html>

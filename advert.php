4d03d583f7291eda05e40a71049344dcf37a831d
DivyanshuRawalLab <divyanshurawallab@gmail.com>
2024-11-06 11:22:26 +0530

    Create Advert Page

---
+++ advert.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Advertising</title>
+    <link rel="stylesheet" href="advert.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+
+         
+
+        <body class="product-template-default single single-product wp-embed-responsive" style="background-color: white;">
+
+        <?php include("header.php"); ?> 
+
+    <div class="section-container single-product">
+        <section class="page-section page-container page-container--full-bleed">
+            <div class="mantel__wrap mantel__wrap--product">
+                <div class="mantel">
+                    <img class="mantel__image"
+                        src="images\laboratory-8895978_1920.png"
+                        alt="Digital Advertising">
+                </div>
+                <div class="mantel__detail">
+                    <div class="mantel__interior">
+                        <div class="mantel__detail-image">
+                            <img src="images\ai-generated-8679778_1920.png"
+                                alt="Digital Advertising">
+                        </div>
+                        <div class="mantel__content">
+                            <h1 class="mantel__detail-title">Digital Advertising</h1>
+                            <div class="mantel__detail-image">
+                                <img src="images\dna-8829661_1920.jpg"
+                                    alt="Digital Advertising">
+                            </div>
+                            <div class="mantel__detail-desc">
+                                <p></p>
+                                <p>Showcase your brand to our audience of scientists and health professionals with
+                                    eye-catching, cost-effective, and measurable digital advertising.</p>
+                                <p></p>
+                            </div>
+                            <a href="" class="mantel__link">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="page-section page-container page-container--slim">
+            <div class="single-product__content wysiwyg">
+                <h2>Our digital ad formats, designed to support your KPIs</h2>
+                <p>Our reader-friendly ad formats offer a beautiful canvas for showcasing your messaging, supporting
+                    brand awareness, product launches, event registrations, recruitment, and more — with the opportunity
+                    to add video and rich media.</p>
+                <div id="attachment_4886" style="width: 760px" class="wp-caption alignleft"><a href="">Download our ad
+                        formats and specs – open “Additional Info”</a></p>
+                </div>
+                <hr>
+                <h2>55 million monthly users</h2>
+                <p><img decoding="async" class="alignright size-full wp-image-2108"
+                        src="images\dna-8829661_1920.jpg"
+                        alt="Advertise on xana   online platforms" width="225" height="225"
+                        srcset="https://partnerships. xana  .com/wp-content/uploads/2017/11/Advertise-on- xana  -online-platforms.jpg 225w, https://partnerships. xana  .com/wp-content/uploads/2017/11/Advertise-on- xana  -online-platforms-150x150.jpg 150w"
+                        sizes="(max-width: 225px) 100vw, 225px">From pioneering scientists to dedicated health
+                    professionals, agenda-setting policymakers, and curious minds — reach our extraordinary global
+                    audiences and benefit from the credibility that comes with appearing alongside our trusted brands —
+                    including  xana, BMC, and Scientific American.</p>
+                <p>Discover more about our audiences:</p>
+                <ul>
+                    <li><strong><a href="" rel="noopener noreferrer">Life sciences and physical sciences
+                                audiences</a></strong></li>
+                    <li><strong><a href="" rel="noopener noreferrer">Healthcare and clinical audiences</a></strong></li>
+                </ul>
+                <hr>
+                <h2>Target your desired audience</h2>
+                <p>Leverage our privacy-forward and precise targeting technology.</p>
+                <p><img decoding="async" class="alignleft wp-image-2109" src="">audience segment</a></strong></p>
+                <p><strong><img loading="lazy" decoding="async" class="alignleft wp-image-2110"
+                            src="images\target-158129_1920.png"
+                            alt="Keyword targeting" width="30" height="30"
+                            srcset="https://partnerships. xana  .com/wp-content/uploads/2017/11/Keyword-targeting-150x150.png 150w, https://partnerships. xana  .com/wp-content/uploads/2017/11/Keyword-targeting.png 300w"
+                            sizes="(max-width: 30px) 100vw, 30px">2. Target by <a href="">keyword, with contextual
+                            advertising</a></strong></p>
+                <p><img loading="lazy" decoding="async" class="alignleft wp-image-2112" src="" target="_blank"
+                        rel="noopener">listed in our catalog</a></strong></p>
+                <hr>
+                <h2><b></b>Audio advertising<b><br>
+                    </b><b></b></h2>
+                <p>Broadcast your brand to 100,000+ weekly listeners of the <a href="" target="_blank"
+                        rel="noopener"> xana  .com podcast</a>,<span style="font-weight: 400;"> discussing the hottest
+                        science topics of the week in this engaging, accessible, and rapidly growing
+                        format.&nbsp;</span></p>
+                <hr>
+                <h2 style="text-align: left;"><b></b>A partnership<b><br>
+                    </b><b></b></h2>
+                <p style="text-align: left;">Feel supported every step of the way. Our team works with you, from
+                    targeting and forecasting to implementation, performance review, and reporting.</p>
+                <h3 style="text-align: center;"><strong><a href="">Talk to our team about digital advertising
+                            &gt;&gt;</a></strong></h3>
+            </div>
+        </section>
+        <section class="page-section" id="additional-info">
+            <div class="additional-info page-container page-container--slim page-container--no-margin"
+                data-js-component="additionalInfo">
+                <div class="content-deck__header title-bar__wrap">
+                    <div class="content-deck__title title-bar__text"></div>
+
+                    <img src="images/teamwork-5508359_1920.png"
+                        alt="Plus Symbol" class="additional-info__plus">
+
+                    <img src="images\structure-1473346_1920.jpg"
+                        alt="Minus Symbol" class="additional-info__minus hidden">
+                </div>
+
+                <div class="link-column " style="display: none;">
+                    <ul class="link-column__list">
+                        <li class="link-column__list-item link-column__list-item--pdf"><a target="_blank"
+                                href="https://partnerships. xana  .com/wp-content/uploads/2023/10/Banner-Specs.pdf">Banner
+                                Specifications </a></li>
+                        <li class="link-column__list-item link-column__list-item--pdf"><a target="_blank"
+                                href="https://partnerships. xana  .com/wp-content/uploads/2023/10/Rich-Media-Specs.pdf">Rich
+                                Media Banner Specifications</a></li>
+                        <li class="link-column__list-item link-column__list-item--pdf"><a target="_blank"
+                                href="https://partnerships. xana  .com/wp-content/uploads/2024/05/Rich-Media-Banner-Examples- xana  .pdf">Examples
+                                of Rich Media Banner Ads</a></li>
+                        <li class="link-column__list-item link-column__list-item--pdf"><a target="_blank"
+                                href="https://partnerships. xana  .com/wp-content/uploads/2018/11/Digital_Design_Request_Form.pdf">Digital
+                                Design Request Form</a></li>
+                        <li class="link-column__list-item link-column__list-item--pdf"><a target="_blank"
+                                href="https://partnerships. xana  .com/wp-content/uploads/2023/10/xana  -Advertising-Terms-and-Conditions_EMEAI.pdf">EMEAI
+                                Advertising Terms &amp; Conditions</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section class="page-section">
+            <div
+                class="page-container page-section--bg-grey page-container--no-margin page-container--full-bleed related-products">
+                <div class="related-product__heading">
+                    <div class="related-products__title">Our digital advertising works best as part of a package</div>
+                    <div class="related-products__subtitle">Communicate with your audience via their preferred channels
+                    </div>
+                </div>
+                <div>
+                    <div class="content-insert">
+                        <div class="content-insert__wrap">
+                            <div class="content-insert__interior">
+                                <div class="content-insert__image">
+                                    <img src="images\marketing-5187244_1920.png"
+                                        alt="">
+                                </div>
+                                <div class="content-insert__copy">
+                                    <div class="content-insert__copy-title">Branded Content</div>
+                                    <div class="content-insert__copy-desc" ?="">Combine your organization’s message with
+                                        our expert journalism to create powerfully engaging stories about your STEM
+                                        research, products, or recruitment — connecting you with  xana  's international
+                                        scientific community.</div>
+                                    <a class="content-insert__copy-button"
+                                        href="https://partnerships. xana  .com/product/branded-content-native-advertising/">Learn
+                                        More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="content-insert">
+                            <div class="content-insert__wrap">
+                                <div class="content-insert__interior">
+                                    <div class="content-insert__image">
+                                        <img src="images\email-4284157_1920.png"
+                                            alt="">
+                                    </div>
+                                    <div class="content-insert__copy">
+                                        <div class="content-insert__copy-title">Email Marketing</div>
+                                        <div class="content-insert__copy-desc" ?="">Use email to communicate directly
+                                            with our international subscribers in science and healthcare — promoting
+                                            your brands, products, events, white papers, and more.</div>
+                                        <a class="content-insert__copy-button"
+                                            href="">Learn
+                                            More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="content-insert">
+                                <div class="content-insert__wrap">
+                                    <div class="content-insert__interior">
+                                        <div class="content-insert__image">
+                                            <img src="images\business-4068346_1920.png"
+                                                alt="">
+                                        </div>
+                                        <div class="content-insert__copy">
+                                            <div class="content-insert__copy-title">Print Advertising</div>
+                                            <div class="content-insert__copy-desc" ?="">Highly visual and authoritative,
+                                                print advertising creates a lasting impression with your audience and
+                                                plays a key role in building awareness at the top of the marketing
+                                                funnel. Place your brand right alongside our influential news and
+                                                research content.</div>
+                                            <a class="content-insert__copy-button"
+                                                href="">Learn More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="related-products__page-link">
+                                <a href="">
+                                    <div class="related-products__page-link-text">Contact us for pricing and other
+                                        opportunities</div>
+                                    <div class="related-products__page-link-arrow"><svg viewBox="0 0 13 21" width="100%"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M12.236 10.278l-1.002-1.029L2.18 0 0 2.058l8.051 8.22L0 18.498l2.18 2.058 9.054-9.249z"
+                                                fill-rule="nonzero"></path>
+                                        </svg>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="page-section page-container page-container--full-width page-container--full-bleed">
+        </section>
+
+
+
+    </div>
+
+
+
+
+
+<?php include("footer.php"); ?>
+
+</body>
+
+</html>

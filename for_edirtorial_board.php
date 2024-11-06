d05f7e55a7869c77f021a9beac4cda4c7b9cef6c
DivyanshuRawalLab <divyanshurawallab@gmail.com>
2024-11-06 16:51:21 +0530

    Create For Editorial Page

---
+++ for_edirtorial_board.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <title>For editorial board member</title>
+	<link rel="stylesheet" href="assets/css/styles.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	
+	
+	
+</head>
+<body>
+
+<?php include("header.php"); ?>
+    
+    <div class="container my-5">
+        <div class="child2">
+			<h2 class="heading1">For Editorial Board Members</h2>
+			<h5 class="my-3">Welcome to the For Editorial Board Members page. This page provides links to resources that will support you in your role for the journal. We recommend bookmarking these links in your browser for future reference. </h5>
+		</div>
+			  <h4 class="heading3">XANA Series Editorial Community Site</h4>
+			  <h5 class="my-3"><em>For training, guidance and resources</em></h5>
+		<nav class="navbar7">
+			<div>
+				<ul>
+				<li><a href="#">https://bmcserieseditors.biomedcentral.com</a></li>
+				</ul>
+			</div>
+		</nav>
+		<h4 class="heading4">Accessing your Editorial Board Member account in our submission systems</h4>
+		<h5 class="head3"><em>To manage submissions you are handling through peer review</em></h5>
+		<nav class="navbar8">
+			<div>
+				<ul>
+				<li style="margin-top: 30px;">Editorial Manager:<a href="#">https://www.biomedcentral.com/p/the-bmc-series-journals/editorial-board</a></li>
+				<li style="margin-top: 10px;">Springer Nature Editorial System hosted at<br> springernature.com: <a href="#"> https://editorial.springernature.com/tasks</a></li>
+				</ul>
+			</div>
+		</nav>
+			<h4 class="heading4">Editorial Responsibilities and FAQs</h4>
+		<h5 class="head3"><em>For information on your responsibilities, expectations and benefits</em></h5>
+		<nav class="navbar8">
+			<div>
+				<ul>
+				<li style="margin-top: 30px;"><a href="#"> https://www.biomedcentral.com/p/the-bmc-series-journals/editorial-board</a></li>
+				</ul>
+			</div>
+			</nav>
+			<h4 class="heading4">XANA Code of Conduct for Editors</h4>
+			<h5 class="head3"><em>The code of conduct you have agreed to adhere to when handling manuscripts on behalf of the<br> journal</em></h5>
+			<nav class="navbar8">
+				<div>
+				<ul>
+				<li style="margin-top: 30px;"><a href="#"> https://www.springernature.com/gp/editors/code-of-conduct-journals</a></li>
+				</ul>
+				</div>
+		</nav>
+	</div>
+
+
+
+        
+	<?php include("footer.php"); ?>
+	   
+</body>
+</html>
+
+</script>

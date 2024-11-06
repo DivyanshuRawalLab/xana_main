a74cd5d542b7d119a67e72ef9e197d3a9116fecc
DivyanshuRawalLab <divyanshurawallab@gmail.com>
2024-11-06 16:51:56 +0530

    Create For Refrees Page

---
+++ for_referees.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>For Referees</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body >
+
+  <?php include("header.php"); ?>
+    
+
+<div class="container my-5"> 
+
+            
+
+
+    <div class="grid grid-9 last mq640-grid-12 mq640-last mt30 mq640-mt0">
+        <div class="container cleared container-type-title" data-container-type="title">
+            <h1 class="small-space-below">For Referees</h1>
+        </div>
+        <div class="container cleared container-type-body-content wysiwyg"
+            data-container-type="body-content">
+            <h4 id="Policies">Policies and Processes</h4>
+            <p>All you need to know when peer reviewing a manuscript for <em> xana  </em>. Please read this section
+                before submitting a report.</p>
+            <h4 id="How">How to Write a Report</h4>
+            <p>Guidance on writing an informative referee report in accordance with the requirements of the editors of
+                <em> xana  </em>.</p>
+            <h4 id="To">How to Submit a Report</h4>
+            <p>Information on how
+                    to submit your referee report to the editors of <em> xana  </em>.</p>
+            <h4 id="Revisions">Revisions and Feedback</h4>
+            <p>Information on your involvement as
+                    peer reviewer in the revision process, and on feedback from the editors of
+                <em> xana  </em>.</p>
+
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+<?php include("footer.php"); ?>
+    
+
+   
+
+</body>
+
+</html>

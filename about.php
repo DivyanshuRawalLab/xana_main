a62a7f4d16bbea052afaeeefcfef09be40898a01
DivyanshuRawalLab <divyanshurawallab@gmail.com>
2024-11-06 11:09:24 +0530

    Create About Us Page

---
+++ about.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/assets\css\styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php include("header.php"); ?>
+	
+			
+		
+			<div class="container my-5">
+                <h2 class="text-center mt-4">About XANA</h2>
+				  <div class="my-3 fs-5">XANA is here to serve the research community by publishing its most significant discoveries—findings that advance knowledge and address some of the greatest challenges that we face as a society today. Our journals publish not only primary research but also reviews, critical comment, news and analysis.</div>
+
+				 <div class="my-3 fs-5">From  xana —the leading international weekly journal of science first published in 1869—to selective subject-specific subscription journals including  xana  Genetics and  xana  Physics and broad open-access journals such as  xana  Communications and Scientific Reports, there is a home for your research within our family of journals. We're committed to providing more options for authors in the open access arena too. The Communications journals, a group of high-quality, selective, open access multidisciplinary journals, published their first articles in 2018. Our  xana  Reviews titles showcase authoritative, accessible and significant review content. High-quality graphics and enhanced content provide context and explanation, whatever your discipline.</div>
+				 
+				  <div class="my-3 fs-5">Every month, nine million people read news, analysis and commentary on xana.com We are committed to developing the next generation of scientists through our backing for the  xana  Awards for Mentoring in Science, the John Maddox Prize for Standing up for Science and the  xana  Research Awards for Inspiring and Innovating Science.</div>
+				 
+				  <h4>Journal metrics</h4>
+				  <p>Information on peer review performance and citation metrics for the  xana  Portfolio journals.</p>
+				  <h4>Press</h4>	
+				  <p>For enquiries from science journalists and writers regarding content in the XANA Portfolio journals, and corporate press enquiries about  xana  Portfolio's publishing activities.</p>
+				 <h4>Careers</h4>
+				  <p>The career site of  Xana , including current opportunities within our offices world-wide.</p>
+				  
+				 <h4>Our parent company</h4>
+				  
+				  <p>XANA is part of xana . The main shareholders of xana  are Holtzbrinck Publishing Group and BC Partners and details about corporate governance and board memberships can be found here.</p>
+				  
+				 <h4>Offices</h4>
+				  
+				  <p>XANA has principal offices in London, New York, Berlin, Shanghai and Tokyo, and offices in cities worldwide.</p>
+			</div>
+
+
+			<?php include("footer.php"); ?>
+	  
+</body>
+</html>

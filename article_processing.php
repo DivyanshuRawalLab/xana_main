9728548103a3495d8a810daa1921b0be0cbc892c
DivyanshuRawalLab <divyanshurawallab@gmail.com>
2024-11-14 16:49:15 +0530

    Add Article Processing Page

---
+++ article_processing.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link rel="stylesheet" href="assets\css\article_processing.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <title>Article processing charges</title>
+</head>
+<body>
+    <?php include("header.php"); ?>
+	   <div class="box3">
+		
+	</nav>
+	   </div>
+       <div id="parent">
+        <div class="child1">
+            <header>
+                <nav class="navbar3">
+                  <div>
+                    <ul>
+                    <li style="font-weight: bold;"><a href="#" style="margin-left: -30px;">Benefits of <br><span style="margin-left: -25px;">publishing with us</span></a></li>
+                   <li> <a href="#" style="margin-left: 30px;">Find the right journal</a></li>
+                    <li><a href="#" style="margin-left: 0px;">Editorial policies</a></li>
+                   <li> <a href="#" style="margin-left: -50px;">ORCiD</a></li>
+                    </ul>
+                  </div>
+                </nav>
+                <nav class="navbar4">
+                    <div>
+                      <ul>
+                      <li style="padding-top: 5px;"><a href="#" style="text-decoration: none;margin-left: -40px;">&#9660; <span style="text-decoration: underline;">Articles processing <br><span style="margin-left: -90px;">charges</span></span></a></li>
+                      <li><a href="#" style="margin-left: 30px;">Peer review process</a></li>
+                      <li><a href="#" style="margin-left:-18px;">Supplements</a></li>
+                      <li><a href="#" style="margin-left: -10px;">Research data</a></li>
+                      <li><a href="#" style="margin-left: 30px;">Indexing , archieving <br><span style="margin-left: 20px;">and access to data</span></a></li>
+                      </ul>
+                    </div>
+                  </nav>
+              </header>
+			  <nav class="navbar3" style="width: 170px;">
+				<div>
+				  <ul style="margin-left: 10px;">
+                    <li style="padding-top: 5px;"><a href="#" style="text-decoration: none;margin-left: -40px;">&#9660; <span style="text-decoration: underline;">Writing resources</a></li>
+				  <li><a href="#" style="margin-left: -9px;">Copyright and<br><span style="margin-left: -55px;">license</span></a></li>
+				  </ul>
+				</div>
+			  </nav>
+        </div>
+        <div class="child2">
+			<h2 class="heading1">Article processing charges</h2>
+            <nav class="navbar5">
+				<div>
+				  <ul>
+				  <li style="margin-top: 20px; list-style: none;"><a href="#"></a>All articles published in our journals are open access and freely available online, immediately <br> upon publication. This is made possible by an article-processing charge (APC) that covers the <br> range of publishing services we provide. This includes provision of online tools for editors and <br> authors, article production and hosting, liaison with abstracting and indexing services, and <br> customer services. The APC, payable when your manuscript is editorially accepted and before <br> publication, is charged to either you, or your funder, institution or employer.</li>
+                  <h2 class="heading1" style="margin-left:-30px; margin-top:20px">Frequently-asked questions (FAQ)</h2>
+                  <h3>Who is responsible for making or arranging the payment?</h3>
+				  <li style="margin-top: 20px;"><a href="#"></a>At XANA, we have always supported innovation in peer review and were one of the first <br> publishers to truly open up peer review in 1999. We are leading on new initiatives to further <br> develop peer review, and suggest ways to improve the process in this report. In keeping with <br> BMC’s commitment to research in progress, we are partnering with PEERE a trans-disciplinary, <br> cross-sectorial collaboration aiming to improve peer review. </li>
+				  <li style="margin-top: 20px;"><a href="#"></a>We also publish a dedicated journal Research Integrity and Peer Review which publishes research <br> into peer review. Where we can make a difference we will, piloting new approaches and <br> partnering with others. Here are some of our initiatives currently underway, if you have ideas or <br> feedback on how we can continue to improve peer review, please contact us.</li>
+</div> </div> </div>
+      <?php include("footer.php"); ?>
+
+</body>
+</html>
+<script>
+	document.getElementById('back-to-top').addEventListener('click', function() {
+  // Scroll to the top of the page
+  window.scrollTo(0, 0);
+});
+document.getElementById('back-to-top1').addEventListener('click', function() {
+  // Scroll to the top of the page
+  window.scrollTo(0, 0);
+});
+</script>
